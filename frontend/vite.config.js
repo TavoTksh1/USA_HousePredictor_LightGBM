378c022d30f9c2087054dd240deddf0c8bd1e800
TavoTksh1 <tavo.tkshi@gmail.com>
2026-03-06 23:10:08 -0500

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // IMPRESCINDIBLE para acceder desde afuera
     port: 5173,
     proxy: {
       '/predict': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000', // Apunta al FastAPI que corre en la misma EC2
         changeOrigin: true,
+        secure: false,
       },
     },
   },
